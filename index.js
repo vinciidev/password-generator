9ca580e53bd64b0d9dec11446d400e4589cf7d12
Pedro Vinicius <vinciidev05@gmail.com>
2024-01-22 02:26:05 -0300

    Add letter counter, Added Password Variation Button

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 let passwordLength = 16
 const inputEl = document.querySelector("#password")
 
+const upperCaseCheckEl = document.querySelector("#uppercase-check")
+const numberCheckEl = document.querySelector("#number-check")
+const symbolCheckEl = document.querySelector("#symbol-check")
+
 
 function generatePassword() {
-   const chars = "abcdefghjkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789?!@&*"
-   let password = " "
+    let password = " "
+    let chars = "abcdefghjkmnpqrstuvwxyz"
+    
+    const upperCaseChars ="ABCDEFGHJKLMNPQRSTUVWXYZ"
+    const numberChars = "0123456789"
+    const symbolChars = "?!@&*"
+
+
+    if(upperCaseCheckEl.checked) {
+        chars += upperCaseChars
+    }
+    
+    if(numberCheckEl.checked) {
+        chars += numberChars
+    }
+   
+    if(symbolCheckEl.checked) {
+        chars += symbolChars
+    }
+    
+
 
    for (let i = 0;  i < passwordLength; i++) {
     const randomNumber = Math.floor(Math.random() * chars.length)
@@ -21,10 +44,17 @@ function generatePassword() {
 const passwordLenghtEl = document.querySelector("#password-length")
 passwordLenghtEl.addEventListener("input", function() {
     passwordLength = passwordLenghtEl.value
+    document.querySelector('#password-length-text').innerText = passwordLength
     generatePassword()
 })
+
+    upperCaseCheckEl.addEventListener("click", generatePassword)
+    numberCheckEl.addEventListener("click", generatePassword)
+    symbolCheckEl.addEventListener("click", generatePassword)
 
     document.querySelector("#copy-2").addEventListener("click", copy)
     document.querySelector("#copy-1").addEventListener("click", copy)
 
     generatePassword()
+
+    
